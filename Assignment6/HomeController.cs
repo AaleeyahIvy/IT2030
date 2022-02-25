bfb5f4bd637e76192f6db4418e044b4e49656908
Aaleeyah Ivy <53322518+AaleeyahIvy@users.noreply.github.com>
2022-02-25 14:31:36 -0500

    Add files via upload

---
+++ Assignment6/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoutingPractice.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Home");
+        }
+
+        public IActionResult Privacy()
+        {
+            return Content("Privacy");
+        }
+
+        public IActionResult Display(string id)
+        {
+            if (id == null)
+            {
+                return Content("No ID supplied.");
+            }
+            else
+            {
+                return Content("ID: " + id);
+            }
+        }
+        [Route("[action]/{start}/{end?}/{message?}")]
+        public IActionResult Countdown(int start, int end, string message = ""){
+            string contentString = "Counting Down: \n";
+            for(int i = start; i >= end; i--){
+                contentString += i+ "\n";
+            }
+            return Content(contentString + message);
+        }
+    }
+}

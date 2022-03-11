8650adc50634e59188021ebb0881049ca65dfa39
Aaleeyah Ivy <53322518+AaleeyahIvy@users.noreply.github.com>
2022-03-11 13:27:10 -0500

    Add files via upload

---
+++ Assignment8/ProductListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace GuitarShop.Models
+{
+    public class ProductListViewModel
+    {
+        public List<Category> Categories { get; set; }
+        public List<Product> Products { get; set; }
+        public string SelectedCategory { get; set; }
+        public string CheckActiveCategory(string category) =>
+            category == SelectedCategory ? "active" : "";
+
+    }
+}

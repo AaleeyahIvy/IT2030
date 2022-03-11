d27360ead25838fb32b790f1ebc77ee9041fd3f2
Aaleeyah Ivy <53322518+AaleeyahIvy@users.noreply.github.com>
2022-03-11 13:27:43 -0500

    Add files via upload

--- Assignment8/ProductController.cs
+++ Assignment8/ProductController.cs
@@ -1,30 +1,33 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using GuitarShop.Models;
 
-namespace GuitarShop.Controllers
+namespace GuitarShop.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class ProductController : Controller
     {
         private ShopContext context;
+        private List<Category> categories;
 
         public ProductController(ShopContext ctx)
         {
             context = ctx;
+            categories = context.Categories
+                    .OrderBy(c => c.CategoryID)
+                    .ToList();
         }
 
         public IActionResult Index()
         {
-            return RedirectToAction("List", "Product");
+            return RedirectToAction("List");
         }
 
-        [Route("[controller]s/{id?}")]
+        [Route("[area]/[controller]s/{id?}")]
         public IActionResult List(string id = "All")
         {
-            var categories = context.Categories
-                .OrderBy(c => c.CategoryID).ToList();
-
             List<Product> products;
             if (id == "All")
             {
@@ -38,7 +41,10 @@ namespace GuitarShop.Controllers
                     .OrderBy(p => p.ProductID).ToList();
             }
 
-            // use ViewBag to pass data to view
+            // use ViewBag to pass category data to view
+            ViewBag.Categories = categories;
+            ViewBag.SelectedCategoryName = id;
+
             var model = new ProductListViewModel
             {
                 Categories = categories,
@@ -50,21 +56,76 @@ namespace GuitarShop.Controllers
             return View(model);
         }
 
-        public IActionResult Details(int id)
+        [HttpGet]
+        public IActionResult Add()
         {
-            var categories = context.Categories
-                .OrderBy(c => c.CategoryID).ToList();
+            // create new Product object
+            Product product = new Product();                // create Product object
+            product.Category = context.Categories.Find(1);  // add Category object - prevents validation problem
 
-            Product product = context.Products.Find(id);
-
-            string imageFilename = product.Code + "_m.png";
-
-            // use ViewBag to pass data to view
+            // use ViewBag to pass action and category data to view
+            ViewBag.Action = "Add";
             ViewBag.Categories = categories;
-            ViewBag.ImageFilename = imageFilename;
 
-            // bind product to view
+            // bind product to AddUpdate view
+            return View("AddUpdate", product);
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            // get Product object for specified primary key
+            Product product = context.Products
+                .Include(p => p.Category)
+                .FirstOrDefault(p => p.ProductID == id);
+
+            // use ViewBag to pass action and category data to view
+            ViewBag.Action = "Update";
+            ViewBag.Categories = categories;
+
+            // bind product to AddUpdate view
+            return View("AddUpdate", product);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                if (product.ProductID == 0)           // new product
+                {
+                    context.Products.Add(product);
+                }
+                else                                  // existing product
+                {
+                    context.Products.Update(product);
+                }
+                context.SaveChanges();
+                TempData["UserMessage"] = $"You added the product : {product.Name}"; 
+                return RedirectToAction("List");
+            }
+            else
+            {
+                ViewBag.Action = "Save";
+                ViewBag.Categories = categories;
+                return View("AddUpdate", product);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            Product product = context.Products
+                .FirstOrDefault(p => p.ProductID == id);
             return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Product product)
+        {
+            context.Products.Remove(product);
+            context.SaveChanges();
+            return RedirectToAction("List");
         }
     }
 }

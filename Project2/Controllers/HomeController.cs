a5852e7565caa11910c69e4e47335563e94a3296
Aaleeyah Ivy <53322518+AaleeyahIvy@users.noreply.github.com>
2022-05-05 13:57:30 -0400

    Add files via upload

---
+++ Project2/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PriceQuoteApp.Models;
+
+namespace PriceQuoteApp
+{
+    public class HomeController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.DiscountAmount = 0;
+            ViewBag.Total = 0;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(PriceQuotationModel price)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.DiscountAmount = price.CalDiscount((double)price.Subtotal, (double)price.DiscountPercent);
+                ViewBag.Total = price.CalTotal((double)price.Subtotal, (double)price.DiscountAmount);
+            }
+            else
+            {
+                ViewBag.DiscountAmount = 0;
+                ViewBag.Total = 0;
+            }
+            return View("Index", price);
+        }
+
+    }
+}
---
+++ Project2/Models/PriceQuotationModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PriceQuoteApp.Models
+{
+    public class PriceQuotationModel
+    {
+        [Required(ErrorMessage = "Please enter a subtotal!")]
+        [Range(1, 10000, ErrorMessage = "Please enter a number between 1 - 10,000!")]
+        public double? Subtotal { get; set; }
+
+        [Required(ErrorMessage = "Please enter a discount percentage!")]
+        [Range(1, 100, ErrorMessage = "Please enter a number between 1 - 100!")]
+        public double? DiscountPercent { get; set; }
+        public double DiscountAmount { get; set; } = 0;
+        public double Total { get; set; } = 0;
+
+        public double CalTotal(double subtotal, double discountAmount)
+        {
+            double total = subtotal - discountAmount;
+            return total;
+        }
+
+        public double CalDiscount(double subtotal, double percent)
+        {
+            double DiscountAmount = (subtotal * percent) / 100;
+            return DiscountAmount;
+
+        }
+    }
+}
